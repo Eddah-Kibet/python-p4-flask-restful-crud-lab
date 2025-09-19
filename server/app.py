e3eb3f774940f26b3c9645258922219e9ee9c775
Eddah kibet <eddahkibet20@gmail.com>
2025-09-19 16:18:11 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -46,6 +46,29 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = db.session.get(Plant, id)
+
+        data = request.get_json()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = db.session.get(Plant, id)
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('Successfully deleted', 204)
+        else:
+            return make_response({'error': 'Plant not found'}, 404)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
